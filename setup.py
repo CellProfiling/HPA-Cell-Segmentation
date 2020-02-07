8d35262e4f056adda8a771634fd6c248a6baa5c2
Wei Ouyang <oeway007@gmail.com>
2020-02-07 12:34:48 +0100

    fix python_requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     name='hpacellseg',
     version='0.1.2',
     author='Hao Xu',
-    python_requirements='>3.6.8',
+    python_requires='>3.6.8',
     author_email='hao.xu@scilifelab.se',
     description='initially for hpa cell segmentation',
     url='https://github.com/CellProfiling/HPA-Cell-Segmentation',
