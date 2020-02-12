8f959ff476a3e1936bf8a46d66487989fe24e106
Fredric Johansson <github@fredricj.se>
2020-02-12 10:55:25 +0100

    Add --version flag, v2 (#7)
    * Add --version flag
    
    * Add MANIFEST.in to include VERSION file in the dist

--- hpacellseg/__main__.py
+++ hpacellseg/__main__.py
@@ -1,5 +1,6 @@
 import click
 from skimage.io import imsave
+from hpacellseg import __version__
 from hpacellseg.hpacellseg import HPACellSeg
 
 @click.command()
@@ -9,6 +10,7 @@ from hpacellseg.hpacellseg import HPACellSeg
 @click.option('--nuclei_mask', default=False, help='The output file path after nuclei mask prediction')
 @click.option('--cell_model', prompt="cell model path", default="./cell_model.pth", help='The model path of cell model')
 @click.option('--nuclei_model', prompt="nuclei model path", default="./nuclei_model.pth", help='The model path of nuclei model')
+@click.version_option(__version__, message="%(version)s")
 def main(cell_channel, nuclei_channel, nuclei_model, cell_model, cell_mask, nuclei_mask):
     cell_label, nuclei_label = HPACellSeg(cell_channel, nuclei_channel, nuclei_model, cell_model).label_mask()
     imsave(cell_mask, cell_label)
