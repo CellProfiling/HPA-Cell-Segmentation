0aa11baee0611cf54e93a4611830ab63b38304ac
hao <xuhaonju@gmail.com>
2020-05-12 15:40:59 +0200

    support er_channel selection in __main__ func

--- hpacellseg/__main__.py
+++ hpacellseg/__main__.py
@@ -11,6 +11,11 @@ from PIL import Image
     help="The image path of microtubules",
 )
 @click.option(
+    "--er_channel",
+    default=None,
+    help="The image path of ER",
+)
+@click.option(
     "--nuclei_channel",
     prompt="nuclei image path",
     help="The image path of nuclei"
@@ -40,13 +45,18 @@ from PIL import Image
 @click.version_option(__version__, message="%(version)s")
 def main(
     cell_channel,
+    er_channel,
     nuclei_channel,
     nuclei_model,
     cell_model,
     cell_mask,
     nuclei_mask
 ):
-    image_channels = [cell_channel, None, nuclei_channel]
+    image_channels = [cell_channel, er_channel, nuclei_channel]
+    if er_channel:
+        # rename the cell model path, to avoid using the save model that does
+        # not support 3 channel segmentation prediction
+        cell_model = cell_model[:-4] + "_3ch.pth"
     cell_label, nuclei_label = HPACellSeg(
         image_channels, nuclei_model, cell_model
     ).label_mask(scale_factor=0.25)
