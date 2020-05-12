ae659fcdc2608a7b20f011b1232f294ba67adcf3
haoxu <32097914+haoxusci@users.noreply.github.com>
2020-05-12 15:43:18 +0200

    support 3-channel segmentation mask generation (#11)
    * Update requirements.txt
    
    * Include er channel (#10)
    
    * support 3 channels
    
    * black
    
    * place holder for 3channel model
    
    * correct main func
    
    * add model path of 3rd channel
    
    * fix check
    
    * image channels, could exclude/include er channel
    
    * change version number and format the code
    
    * support er_channel selection in __main__ func

--- hpacellseg/__main__.py
+++ hpacellseg/__main__.py
@@ -3,20 +3,67 @@ from hpacellseg import __version__
 from hpacellseg.hpacellseg import HPACellSeg
 from PIL import Image
 
+
 @click.command()
-@click.option('--cell_channel', prompt="microtubules image path", help='The image path of microtubules')
-@click.option('--nuclei_channel', prompt="nuclei image path", help='The image path of nuclei')
-@click.option('--cell_mask', prompt="output file path of lableled mask", help='The output file path after cell mask prediction')
-@click.option('--nuclei_mask', default=False, help='The output file path after nuclei mask prediction')
-@click.option('--cell_model', prompt="cell model path", default="./cell_model.pth", help='The model path of cell model')
-@click.option('--nuclei_model', prompt="nuclei model path", default="./nuclei_model.pth", help='The model path of nuclei model')
+@click.option(
+    "--cell_channel",
+    prompt="microtubules image path",
+    help="The image path of microtubules",
+)
+@click.option(
+    "--er_channel",
+    default=None,
+    help="The image path of ER",
+)
+@click.option(
+    "--nuclei_channel",
+    prompt="nuclei image path",
+    help="The image path of nuclei"
+)
+@click.option(
+    "--cell_mask",
+    prompt="output file path of lableled mask",
+    help="The output file path after cell mask prediction",
+)
+@click.option(
+    "--nuclei_mask",
+    default=False,
+    help="The output file path after nuclei mask prediction",
+)
+@click.option(
+    "--cell_model",
+    prompt="cell model path",
+    default="./cell_model.pth",
+    help="The model path of cell model",
+)
+@click.option(
+    "--nuclei_model",
+    prompt="nuclei model path",
+    default="./nuclei_model.pth",
+    help="The model path of nuclei model",
+)
 @click.version_option(__version__, message="%(version)s")
-def main(cell_channel, nuclei_channel, nuclei_model, cell_model, cell_mask, nuclei_mask):
-    cell_label, nuclei_label = HPACellSeg(cell_channel, nuclei_channel, nuclei_model, cell_model).label_mask()
+def main(
+    cell_channel,
+    er_channel,
+    nuclei_channel,
+    nuclei_model,
+    cell_model,
+    cell_mask,
+    nuclei_mask
+):
+    image_channels = [cell_channel, er_channel, nuclei_channel]
+    if er_channel:
+        # rename the cell model path, to avoid using the save model that does
+        # not support 3 channel segmentation prediction
+        cell_model = cell_model[:-4] + "_3ch.pth"
+    cell_label, nuclei_label = HPACellSeg(
+        image_channels, nuclei_model, cell_model
+    ).label_mask(scale_factor=0.25)
     Image.fromarray(cell_label).save(cell_mask, bits=16)
     if nuclei_mask:
         Image.fromarray(nuclei_label).save(nuclei_mask, bits=16)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
--- hpacellseg/hpacellseg.py
+++ hpacellseg/hpacellseg.py
@@ -24,39 +24,47 @@ class HPACellSeg:
 
     def __init__(
         self,
-        cell_channel,
-        nuclei_channel,
+        image_channels,  # ['microtubules.png', 'er.png/None', 'nuclei.png'] or list
         nuclei_model="./nuclei_model.pth",
         cell_model="./cell_model.pth",
         batch_process=False,
     ):
+        cell_channel, channel2nd, nuclei_channel = image_channels
         self.batch_process = batch_process
         if self.batch_process:
             assert isinstance(cell_channel, list)
             assert isinstance(nuclei_channel, list)
-            assert len(cell_channel) == len(nuclei_channel)
+            if channel2nd:
+                assert isinstance(channel2nd, list)
+                assert len(cell_channel) == len(channel2nd) == len(nuclei_channel)
+            else:
+                assert len(cell_channel) == len(nuclei_channel)
         else:
             assert isinstance(cell_channel, str)
             assert isinstance(nuclei_channel, str)
             cell_channel = [cell_channel]
+            if channel2nd:
+                assert isinstance(channel2nd, str)
+                channel2nd = [channel2nd]
             nuclei_channel = [nuclei_channel]
         cell_channel = [os.path.expanduser(item) for _, item in enumerate(cell_channel)]
         nuclei_channel = [
             os.path.expanduser(item) for _, item in enumerate(nuclei_channel)
         ]
 
-        self.cell_channel = cell_channel
-        self.nuclei_channel = nuclei_channel
-
-        mt_data = list(map(lambda x: imageio.imread(x), self.cell_channel))
-        nuclei_data = list(map(lambda x: imageio.imread(x), self.nuclei_channel))
-        empty_channel = [
-            np.zeros(item.shape, dtype=item.dtype) for _, item in enumerate(mt_data)
-        ]
+        mt_data = list(map(lambda x: imageio.imread(x), cell_channel))
+        nuclei_data = list(map(lambda x: imageio.imread(x), nuclei_channel))
+        if channel2nd:
+            channel2nd = [os.path.expanduser(item) for _, item in enumerate(channel2nd)]
+            second_channel = list(map(lambda x: imageio.imread(x), channel2nd))
+        else:
+            second_channel = [
+                np.zeros(item.shape, dtype=item.dtype) for _, item in enumerate(mt_data)
+            ]
         self.cell_imgs = list(
             map(
                 lambda item: np.dstack((item[0], item[1], item[2])),
-                list(zip(mt_data, empty_channel, nuclei_data)),
+                list(zip(mt_data, second_channel, nuclei_data)),
             )
         )
 
@@ -71,21 +79,30 @@ class HPACellSeg:
         if not os.path.exists(cell_model):
             os.makedirs(os.path.dirname(cell_model), exist_ok=True)
             print("Downloading cell segmentation model...")
-            cell_model_url = (
-                "https://kth.box.com/shared/static/he8kbtpqdzm9xiznaospm15w4oqxp40f.pth"
+            if channel2nd:  # place holder for 3channel model
+                cell_model_url = (
+                "https://kth.box.com/shared/static/hl2vuyi1lugywk6fr0drdz48w90gniyv.pth"
             )
+            else:
+                cell_model_url = (
+                    "https://kth.box.com/shared/static/he8kbtpqdzm9xiznaospm15w4oqxp40f.pth"
+                )
             download_with_url(cell_model_url, cell_model)
         self.nuclei_model = nuclei_model
         self.cell_model = cell_model
 
-    def label_mask(self, scale_factor=0.5):
+    def label_mask(self, scale_factor=0.25):
         seg = CellSegmentator(
-            self.nuclei_model, self.cell_model, scale_factor=scale_factor, padding=True
+            self.nuclei_model,
+            self.cell_model,
+            scale_factor=scale_factor,
+            padding=True
         )
         cell_masks = seg.label_cells(self.cell_imgs)
         if self.batch_process:
             print(
-                "The return value is list of cell mask data, following the cell_channel images"
+                "The return value is list of cell mask data, following the "
+                "cell_channel images"
             )
         else:
             cell_masks = cell_masks[0]
