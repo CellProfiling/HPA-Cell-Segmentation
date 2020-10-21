a0477316c9860ca4b05421ccf6afffdf3262b66e
Casper Winsnes <cwinsnes92@gmail.com>
2020-10-21 15:56:25 +0200

    Update README

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 pip3 install scikit-image imageio scipy opencv-python pillow==6.2.1
-#conda install pytorch=1.2.0 torchvision=0.4.0 cudatoolkit=10.0 -c pytorch # use this when gpu available
 pip3 install 'git+https://github.com/haoxusci/pytorch_zoo@master#egg=pytorch_zoo'
 
 pip3 install . --upgrade
